04188c8e1df0bd3421c6f69dbf8edcf043e9f130
Lauro C Oliveira <lauro@hostcert.com.br>
2021-04-30 13:55:00 -0300

    Fix code format

--- backend/django_project/project/custom_backends/custom_login.py
+++ backend/django_project/project/custom_backends/custom_login.py
@@ -1,15 +1,23 @@
+"""
+"""
 from django.contrib.auth.backends import BaseBackend
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
-from arquivos.models import TbUser
 import uuid
 import hashlib
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 User = get_user_model()
 
 
 class CustomLogin(BaseBackend):
+    """ """
+
     def hash_password(self, password):
         salt = uuid.uuid4().hex
         return (
@@ -18,19 +26,20 @@ class CustomLogin(BaseBackend):
 
     def check_password(self, hashed_password, user_password):
         password, salt = hashed_password.split(":")
-        return (
-            password
-            == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
-        )
+
+        p = hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+        return password == p
 
     def authenticate(self, request, username=None, password=None):
         # hashed_password = self.hash_password(username + password)
         try:
-            tbuser = TbUser.objects.get(user_code=username)
-        except:
+            tbuser = User.objects.get(user_code=username)
+        except Exception as e:
+            logger.error(e)
             return None
         else:
-            if isinstance(tbuser, (TbUser)):
+            if isinstance(tbuser, (User)):
                 old_pass = password
                 old_pass = username + old_pass
                 if self.check_password(tbuser.password.strip(), old_pass):
@@ -43,8 +52,8 @@ class CustomLogin(BaseBackend):
                         user.save()
                         try:
                             grupo = Group.objects.get(name="operadores")
-                        except:
-                            print("Crie um grupo chamado operadores")
+                        except Exception as e:
+                            logger.error(e)
                             return None
                         else:
                             user.groups.set([grupo.pk])
--- backend/django_project/project/models.py
+++ backend/django_project/project/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+
 # Be careful with related_name and related_query_name
 class BaseModel(models.Model):
+
     created = models.DateTimeField(auto_now=True, verbose_name=_("Data de criação"))
     lastModified = models.DateTimeField(
         auto_now=True, verbose_name=_("Última modificacao")
